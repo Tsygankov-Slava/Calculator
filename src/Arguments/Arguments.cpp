a54079fb808c278d75f11aaf31c8c5610de85391
Tsygankov-Slava <st200399200399@gmail.com>
2022-05-15 01:02:17 +0300

    Updated commands

--- src/Arguments/Arguments.cpp
+++ src/Arguments/Arguments.cpp
@@ -4,11 +4,11 @@ void Arguments::checkArguments(int argc, char *argv[], std::string &exp, File &f
     for (int i = 0; i < argc; ++i) {
         if (std::strcmp(argv[i], "-d") == 0) {
             isDebug = true;
-        } else if (std::strcmp(argv[i], "-variables") == 0) {
+        } else if (std::strcmp(argv[i], "--variables") == 0) {
             file.pathVariables = argv[i + 1];
-        } else if (std::strcmp(argv[i], "-exp") == 0) {
+        } else if (std::strcmp(argv[i], "--exp") == 0) {
             exp = argv[i + 1];
-        } else if (std::strcmp(argv[i], "-history") == 0) {
+        } else if (std::strcmp(argv[i], "--history") == 0) {
             file.pathHistory = argv[i + 1];
         }
     }
